29602bdd73fa916b945a7ee13009d836ac122e03
Jaromír Stanko <jaromir.stanko@aerobtec.com>
2019-10-29 08:39:24 +0100

    changed USART2 rx pin

--- vrs_cvicenie_6_UART/Src/usart.c
+++ vrs_cvicenie_6_UART/Src/usart.c
@@ -38,10 +38,10 @@ void MX_USART2_UART_Init(void)
   
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
   /**USART2 GPIO Configuration  
-  PA2   ------> USART2_TX
-  PA3   ------> USART2_RX 
+  PA2    ------> USART2_TX
+  PA15   ------> USART2_RX
   */
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_2|LL_GPIO_PIN_3;
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_2|LL_GPIO_PIN_15;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
